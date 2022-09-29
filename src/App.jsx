b9fef071fa4b16be2f840d64c7ba73964aa48af2
Tomas <tomibudeguer20@gmail.com>
2022-09-29 18:23:33 -0300

    agregue la consulta a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container} from "react-bootstrap";
-import Frase from './components/Frase';
-import './style.css'
+import { Button, Container } from "react-bootstrap";
+import Frase from "./components/Frase";
+import "./style.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []); //peticion a la API en montaje nada mas (En un principio)
+
+  const consultarAPI = async () => {
+    try {
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      console.log(respuesta);
+      const dato = await respuesta.json();
+      console.log(dato[0]);
+      setPersonaje(dato[0]);
+    } catch (error) {
+      console.log(error);
+      // mostrar un mensaje al ususario
+    }
+  };
+
   return (
     <Container>
       <div className="m-5 text-center">
-      <img
-        src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/download/image.png"
-        alt="the simpsons"
-        className="w-100"
-      />
+        <img
+          src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/download/image.png"
+          alt="the simpsons"
+          className="w-100"
+        />
       </div>
       <div className="text-center">
-      <button className="botonNaranja mb-5">Obtener frase</button>
+        <Button className="botonNaranja mb-5" variant="warning">
+          Obtener frase
+        </Button>
       </div>
-      <Frase></Frase>
+      <Frase personaje={personaje}></Frase>
     </Container>
   );
 }
